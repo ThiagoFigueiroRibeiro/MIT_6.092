a2fccc4087890256474cc7401a57b1c60c2f602f
ThiagoFigueiroRibeiro <thiago.figueiro.ribeiro@gmail.com>
2023-08-30 10:00:23 -0300

    Update assign4_Book.java
    Fixing and old code.

--- assign4_Book.java
+++ assign4_Book.java
@@ -1,34 +1,34 @@
-public class Book {
-String title;
-boolean borrowed;
-// Creates a new Book
-public Book(String bookTitle) {
-    title = bookTitle;
-    borrowed = false;
+/* Back in 2015 I had no idea why the previous version of this code didn't work.
+    Now in 2023 finally fixed it. So, there is this class called Book that has a title and can be borrowed.
+    The, I test if it returns the title and the status of the book*/
+
+class Book { String title; boolean borrowed;
+
+    public Book(String bookTitle) {title = bookTitle; borrowed = false;} // Creates a new Book
+    
+    public void borrow() {borrowed = true;} // Marks the book as rented
+    
+    public void returned() {borrowed = false;} // Marks the book as not rented
+    
+    public boolean isBorrowed() {return borrowed;} // Returns true if the book is rented, false otherwise
+    
+    public String getTitle() {return title;} // Returns the title of the book
 }
-// Marks the book as rented
-public void borrow() {
-    borrowed = true;
-}
-// Marks the book as not rented
-public void returned() {
-    borrowed = false;
-}
-// Returns true if the book is rented, false otherwise
-public boolean isBorrowed() {
-    return borrowed;
-}
-// Returns the title of the book
-public String getTitle() {
-    return title;
-} 
-public static void main(String[] arguments) {
-Book example = new Book("The Da Vinci Code");
-System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-example.borrow();
-System.out.println("Borrowed? (should be true): " + example.isBorrowed());
-example.returned();
-System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-}
-} 
+
+public class Main{ 
+    public static void main(String[] arguments){
+        Book example = new Book("The Da Vinci Code");
+        
+        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+        
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        
+        System.out.println("I'm now borrowing the book."); 
+        example.borrow();       // Borrowing the book
+        
+        System.out.println("Borrowed? (should be true): " + example.isBorrowed()); 
+        
+        System.out.println("I'm now returning the book.");
+        example.returned();     // Returning the book.
+        
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());}}
